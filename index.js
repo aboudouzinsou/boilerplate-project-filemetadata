74e117021f83e2446cd9f39cdf3e14d3fba9f1f7
aboudouzinsou <faboudouzinsou@gmail.com>
2024-09-05 19:39:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,36 @@ var app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+const upload = multer();
+
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const file = req.file;
+  if (!file) {
+      return res.json({ error: 'No file uploaded' });
+  }
 
+  const fileMetadata = {
+      name: file.originalname,
+      type: file.mimetype,
+      size: file.size
+  };
+
+  // Rediriger vers la route /api/fileanalyse avec les métadonnées du fichier
+  res.redirect(`/api/fileanalyse?name=${fileMetadata.name}&type=${fileMetadata.type}&size=${fileMetadata.size}`);
+});
+
+app.get('/api/fileanalyse', (req, res) => {
+  const { name, type, size } = req.query;
+  res.json({
+      name,
+      type,
+      size: parseInt(size)
+  });
+});
 
 
 const port = process.env.PORT || 3000;
